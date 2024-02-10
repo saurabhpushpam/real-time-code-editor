f40dda6df70693c757a0e82490d206f6627a2d05
saurabhpushpam <145956198+saurabhpushpam@users.noreply.github.com>
2024-02-10 19:30:04 +0530

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -78,7 +78,7 @@ const Home = () => {
             <footer>
                 <h4>
                     Built with 💛 &nbsp; by &nbsp;
-                    <a href="https://github.com/codersgyan">Coder's Gyan</a>
+                    <a href="#">Gaurav & Saurabh</a>
                 </h4>
             </footer>
         </div>
